695354b317b9fb27d3a423ab6d7dcf9fc03fd625
adityagupta1435 <49440335+adityagupta1435@users.noreply.github.com>
2024-10-23 00:29:30 +0530

    Added scripts in package.json

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.use("/test", (req, res) => {
+  res.send("Namaste from the server");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on the port 3000");
+});
