8fed61aa3e41cfc6368066b16ebc0600f8454d1e
adityagupta1435 <49440335+adityagupta1435@users.noreply.github.com>
2024-10-24 20:05:40 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ab?c", (req, res) => {
-  res.send({
-    firstname: "Aditya",
-    lastname: "Gupta",
-  });
-});
-
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({
-    firstname: "Aditya",
-    lastname: "Gupta",
-  });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    //route handler
+    next();
+    res.send("Route Handler 1");
+  },
+  (req, res) => {
+    res.send("Route Handler 2");
+  },
+  (req, res) => {
+    res.send("Route Handler 2");
+  }
+);
 app.listen(3000, () => {
   console.log("Server is successfully listening on the port 3000");
 });
