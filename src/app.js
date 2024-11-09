aee15daa82ab6e78144f5c64126200d74282c3a6
adityagupta1435 <gupta.102aditya102@gmail.com>
2024-11-10 02:16:53 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+//Sign up Api
 app.post("/signup", async (req, res) => {
   //Creating a new instance of User model
   const user = new User(req.body);
@@ -14,6 +15,30 @@ app.post("/signup", async (req, res) => {
     res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:", +err.message);
+  }
+});
+
+//Get User by Email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(400).send("User not found!");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+//Feed API - GET / feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
   }
 });
 
